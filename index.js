41f1726fd6873575469b09f2813ca3bc88c7a7e6
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-01-02 12:21:49 +0530

    added new things

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const btnOpen = document.querySelector('.btn-open');
+const btnClose = document.querySelector('.btn-close');
+const menu = document.querySelector('#menu');
 
+btnOpen.addEventListener('click', function () {
+  btnClose.classList.remove('hidden');
+  btnOpen.classList.add('hidden');
+  btnClose.classList.add('block');
+  menu.classList.remove('hidden');
+  menu.classList.add('flex');
+});
+
+btnClose.addEventListener('click', function () {
+  btnClose.classList.add('hidden');
+  btnOpen.classList.remove('hidden');
+  btnClose.classList.remove('block');
+  menu.classList.add('hidden');
+  menu.classList.remove('flex');
+});
+
+menu.addEventListener('click', function () {
+  btnClose.classList.add('hidden');
+  btnOpen.classList.remove('hidden');
+  btnClose.classList.remove('block');
+  menu.classList.add('hidden');
+  menu.classList.remove('flex');
+});
